9a688177a2b11cdb29ee7e27925f7388804bc046
Rajab Natshah <rajabn@gmail.com>
2020-04-13 00:33:33 +0300

    Issue #3126968: Initialize [Vartheme Claro] admin theme

---
+++ js/vartheme-claro.script.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * Behaviors for the Vartheme Claro theme.
+ */
+
+(function ($, _, Drupal, drupalSettings) {
+  'use strict';
+
+  Drupal.behaviors.varthemeClaro = {
+    attach: function (context) {
+      // Vartheme Claro admin JavaScript behaviors goes here.
+    }
+  };
+
+})(window.jQuery, window._, window.Drupal, window.drupalSettings);
