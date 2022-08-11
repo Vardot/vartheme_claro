ef63ee4a5ed39b70919adcd2ecf4d19df8acfac7
git <git@3644742.no-reply.drupal.org>
2022-08-11 15:32:42 +0300

    Issue #3290341 by Project Update Bot: Automated Drupal 10 compatibility fixes for Vartheme Claro admin theme on the 4.0.x branch

--- tests/src/FunctionalJavascript/VarthemeClaroTests.php
+++ tests/src/FunctionalJavascript/VarthemeClaroTests.php
@@ -40,7 +40,7 @@ class VarthemeClaroTests extends WebDriverTestBase {
   /**
    * {@inheritdoc}
    */
-  protected function setUp() {
+  protected function setUp(): void {
     parent::setUp();
 
     $this->drupalLogin($this->rootUser);
